1146765d5f6703399e076e07b7fe194df68f9a8b
Tejas Patelia <tejas0.patelia@loblaw.ca>
2023-09-29 14:40:31 -0400

    Initial Commit

---
+++ Lesson-19-converter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lesson-19-converter
+//
+//  Created by Tejas Patelia on 2023-09-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson-19-converter/Lesson_19_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_19_converterApp.swift
+//  Lesson-19-converter
+//
+//  Created by Tejas Patelia on 2023-09-29.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_19_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
